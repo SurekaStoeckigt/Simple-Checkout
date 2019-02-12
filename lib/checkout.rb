dbdb12ce7eaadaae4a207df64b74006b8678d319
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-02-12 20:15:01 +0000

    Simple-Checkout all tests pass

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,2 +1,50 @@
 class Checkout
-end 
+  attr_reader :items
+
+    def initialize
+      @items = [
+            {name: 'bananas', price: '1'},
+            {name: 'milk', price: '2'}
+              ]
+    end
+
+    def check_price(item)
+     price = item[:price]
+    end
+
+    def add_to_list(item)
+      items.push(item)
+    end
+
+    def find_price(name)
+      items.select{|price| items[:name] == price}
+    end
+
+    def scan_items(items_to_scan)
+      items_to_scan = @items
+      checkout_list = []
+      items_to_scan.each do |item_and_price|
+        checkout_list << item_and_price
+      end
+      checkout_list
+    end
+
+    def price_from_scanned(checkout_list)
+      checkout_list = @items
+      price = 0
+      checkout_list.each do |value|
+      price += value[:price].to_i
+        end
+      price
+      end
+
+    def checkout_items(checkout_list)
+      checkout_list = @items
+      format = ""
+      checkout_list.each do |value|
+        format+= "#{value[:name]} #{value[:price]}"
+      end
+      format
+    end
+
+    end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,4 +2,43 @@ require 'checkout'
 
 describe Checkout do
 
+  it 'observes the price of an item' do
+    checkout = Checkout.new
+    new_item = {name: 'chocolate', price: 2}
+    expect(checkout.check_price(new_item)).to eq(2)
+  end
+
+it 'scans items at check out' do
+checkout = Checkout.new
+list = [
+      {name: 'bananas', price: '1'},
+      {name: 'milk', price: '2'}
+        ]
+expect(checkout.scan_items(list)).to eq checkout.items
+end
+
+it 'returns total for all scanned items' do
+  checkout = Checkout.new
+  list = [
+        {name: 'bananas', price: '1'},
+        {name: 'milk', price: '2'}
+          ]
+    #original = checkout.items
+    total = checkout.price_from_scanned(checkout.items)
+  expect(checkout.price_from_scanned(list)).to eq total
+end
+
+it 'formats prices individually' do
+  checkout = Checkout.new
+  list = [
+        {name: 'bananas', price: '1'},
+        {name: 'milk', price: '2'}
+          ]
+  format = ""
+  list.each do |value|
+  format += "#{value[:name]} #{value[:price]}"
+  end
+  expect(checkout.checkout_items(list)).to eq format
+end
+
 end
